b3a33acd9d0e961a6969b927aaef05c267864b33
ryouhei0622 <seijakunomutou@gmail.com>
2024-02-13 22:23:22 +0900

    feat: herderを作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'base-blue': '#00C4CC',
+      },
+      height: {
+        'header-height': '50px',
+      },
+    },
   },
-  plugins: [],
 }
-
