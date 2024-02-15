95c394e41adf9c6540b9272f8d28c785faa59595
ryouhei0622 <seijakunomutou@gmail.com>
2024-02-15 22:32:34 +0900

    refac

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import './App.css';
-import { Table } from './table';
-import {Company} from './company';
+import { Table } from './Table';
+import {Company} from './Company';
+import { Header } from './Header';
 
 function App() {
   return (
     <div className="Home bg-base-gray">
-      <header className="h-header-height bg-base-blue mb-header-margin text-white text-3xl font-bold flex justify-between items-center">
-        <div>
-          <img src="./bat.svg" alt="Bat" className="inline-block ml-6 my-2 h-6 align-middle" />
-          <span className="inline-block ml-6 mx-4 my-4 h-6 align-middle">SmartHR</span>
-          <span className="text-sm">燈株式会社</span>
-        </div>
-        <span className="mr-6 text-sm">山田 太郎 (A01234)</span>
-      </header>
+      <Header/>
       <div className="flex w-4/5 h-screen mx-auto space-x-header-margin  justify-center">
         <div className="w-table-width h-4/5 mx-header-margin space-y-header-margin bg-base-gray flex flex-col">
             <Table/>
--- src/company.tsx
+++ src/company.tsx
@@ -2,11 +2,11 @@ export const Company = () => {
     return(
         <>
         <div className="my-header-margin bg-company-gray text-white">
-          <img src="./bat.svg" alt="Bat" className="font-bold inline-block ml-2 h-5 align-middle text-white" /> {""}
-          会社
+            <img src="./bat.svg" alt="Bat" className="font-bold inline-block ml-2 h-5 align-middle text-white" /> {""}
+            会社
         </div>
         <div className="border-b border-company-gray w-full">
-          <h1 className="font-bold inline-block">日本株式会社</h1>
+            <h1 className="font-bold inline-block">日本株式会社</h1>
         </div>
         〒123-4567 <br/>東京都会社区会社町1-2-3 <br/>TEL: 03-1234-5678 <br/>法人番号: 1234567890123
         </>
