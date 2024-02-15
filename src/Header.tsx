7b0a6e0f70c5d390f0d28de1ed73b5bcbae99add
ryouhei0622 <seijakunomutou@gmail.com>
2024-02-15 22:32:02 +0900

    refac: headerをcomponentに分けた

---
+++ src/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+    return(
+        <header className="h-header-height bg-base-blue mb-header-margin text-white text-3xl font-bold flex justify-between items-center">
+        <div>
+          <img src="./bat.svg" alt="Bat" className="inline-block ml-6 my-2 h-6 align-middle" />
+          <span className="inline-block ml-6 mx-4 my-4 h-6 align-middle">SmartHR</span>
+          <span className="text-sm">燈株式会社</span>
+        </div>
+        <span className="mr-6 text-sm">山田 太郎 (A01234)</span>
+      </header>
+    )
+}
