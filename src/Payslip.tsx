5fd6318e217cd794266c8db3f6fc122456acc125
ryouhei0622 <seijakunomutou@gmail.com>
2024-02-16 00:11:19 +0900

    feat: 源泉徴収票

--- src/Payslip.tsx
+++ src/Payslip.tsx
@@ -7,16 +7,16 @@ export const Payslip = () => {
         </div>
         {/* 下にリンクされたテキストと下線を追加、リンクされたテキストも左にheader-marginを適用し、下線はリンクなしでも伸ばす */}
         <div className="mt-4 mx-header-margin border-t">
-            <a href="/your-link" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">2024(令和06)年01月1日支給分 給与明細</a>
+            <a href="/your-link" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 hover:underline">2024(令和06)年01月1日支給分 給与明細</a>
         </div>
         <div className="mt-4 mx-header-margin border-t">
-            <a href="/your-link" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">2024(令和06)年02月1日支給分 給与明細</a>
+            <a href="/your-link" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 hover:underline">2024(令和06)年02月1日支給分 給与明細</a>
         </div>
         <div className="mt-4 mx-header-margin border-t">
-            <a href="/your-link" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">2024(令和06)年03月1日支給分 給与明細</a>
+            <a href="/your-link" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 hover:underline">2024(令和06)年03月1日支給分 給与明細</a>
         </div>
         <div dir="rtl" className="mt-4 mx-header-margin">
-            <a href="/your-link" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">給与明細の一覧を見る</a>
+            <a href="/your-link" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 hover:underline">給与明細の一覧を見る</a>
         </div>
         </>
     )
--- src/WithholdingTax.tsx
+++ src/WithholdingTax.tsx
@@ -1,9 +1,12 @@
 export const WithholdingTax = () => {
     return(
         <>
-        <div className="text-3xl flex items-center">
+        <div className="text-3xl flex items-center border-b">
             <img src="./bat.svg" alt="Bat" className="inline-block ml-header-margin mx-header-margin my-header-margin  h-8 align-middle" />
             源泉徴収票
+        </div>
+        <div className="mt-4 mx-header-margin">
+            <a href="/your-link" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 hover:underline">山田 太郎さんの令和05年分 源泉徴収票_12月頑張った人向けに配布</a>
         </div>
         </>
     )
