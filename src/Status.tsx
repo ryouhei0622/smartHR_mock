2f6b0167b08354fd702e25126d05152ebdb03a9f
ryouhei0622 <seijakunomutou@gmail.com>
2024-02-24 17:00:11 +0900

    refac: 申請のアイコンと文字列を揃えた

--- src/Status.tsx
+++ src/Status.tsx
@@ -7,7 +7,7 @@ export const Status = () => {
                 <img src="./bat.svg" alt="Bat" className="inline-block ml-header-margin mx-header-margin my-header-margin h-8 align-middle" />
                 申請の状況
             </div>
-        <button className="mr-header-margin text-sm border border-black py-1 px-3 rounded-md hover:bg-gray-200 base-ease focus:base-focus cursor-pointer shadow-btn-shadow">
+        <button className="flex items-center mr-header-margin text-sm border border-black py-1 px-3 rounded-md hover:bg-gray-200 base-ease focus:base-focus cursor-pointer shadow-btn-shadow">
             <IoMdAddCircle className="inline-block mr-2 h-4 align-middle" />
             新しい申請
         </button>
