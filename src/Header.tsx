e48b711d4727c12efa15512649878b11c829d1c5
ryouhei0622 <seijakunomutou@gmail.com>
2024-02-24 15:37:54 +0900

    refac: headerの右側にアイコンを追加

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,7 +6,10 @@ export const Header = () => {
           <span className="inline-block ml-6 mx-4 my-4 h-6 align-middle">SmartHR</span>
           <span className="text-sm">燈株式会社</span>
         </div>
-        <span className="mr-6 text-sm">山田 太郎 (A01234)</span>
+        <div className="flex items-center">
+          <img src="./bat.svg" alt="Bat" className="inline-block mr-2 my-2 h-6 align-middle" />
+          <span className="mr-6 text-sm">山田 太郎 (A01234)</span>
+        </div>
       </header>
     )
 }
