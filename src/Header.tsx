d8f01700c6f6ed84024901d28208af25f78fa6b1
ryouhei0622 <seijakunomutou@gmail.com>
2024-02-29 22:54:25 +0900

    refac: headerを変更(buttonにできていない)

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,15 +1,31 @@
+import { LuBadgeDollarSign } from "react-icons/lu";
+import { TbSquareNumber5 } from "react-icons/tb";
+import { CiFaceSmile } from "react-icons/ci";
+
 export const Header = () => {
     return(
-        <header className="h-header-height bg-base-blue mb-header-margin text-white text-3xl font-bold flex justify-between items-center">
-        <div>
-          <img src="./bat.svg" alt="Bat" className="inline-block ml-6 my-2 h-6 align-middle" />
-          <span className="inline-block ml-6 mx-4 my-4 h-6 align-middle">SmartHR</span>
-          <span className="text-sm">燈株式会社</span>
-        </div>
+      <header className="h-header-height bg-base-blue mb-header-margin text-white text-3xl font-bold flex justify-between items-center">
+      <div className="flex items-center"> 
+        <LuBadgeDollarSign className="inline-block ml-6 my-2 h-6" />
+        <span className="mr-4">SmartHR</span>
+        <span className="text-sm">燈株式会社</span>
+      </div>
         <div className="flex items-center">
-          <img src="./bat.svg" alt="Bat" className="inline-block mr-2 my-2 h-6 align-middle" />
-          <span className="mr-6 text-sm">山田 太郎 (A01234)</span>
+          <TbSquareNumber5 className="mr-6" />
+          <CiFaceSmile className="mr-2" />
+          <span className="mr-6 text-sm">山田 太郎 (A01234) ▼</span>
         </div>
       </header>
     )
 }
+<header className="h-header-height bg-base-blue mb-header-margin text-white text-3xl font-bold flex justify-between items-center">
+        <div>
+          <LuBadgeDollarSign className="inline-block my-2 h-6 align-middle" />
+          <span className="ml-6 mr-4">SmartHR</span>
+          <span className="text-sm">株式会社</span>
+        </div>
+        <div className="flex items-center">
+          <TbSquareNumber5 className="inline-block mr-2 my-2 h-6 align-middle" />
+          <span className="mr-6 text-sm">山田 太郎 (A01234) ▼</span>
+        </div>
+      </header>
